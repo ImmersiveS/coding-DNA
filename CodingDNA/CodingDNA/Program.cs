586e8b3ecebeaa7fbd8b329b603fe4b14e67cb67
Sergey <seruy5432@gmail.com>
2017-02-11 00:22:12 +0200

    Add initial files

---
+++ CodingDNA/CodingDNA/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDNA
+{
+    class Program
+    {
+        //States where H means high GC content and L - low GC content
+        static char H = 'H';
+        static char L = 'L';
+        //Observations
+        static char A = 'A';
+        static char C = 'C';
+        static char G = 'G';
+        static char T = 'T';
+
+        static void Main(string[] args)
+        {
+            //TODO implement initialization of data and its output
+        }
+
+        public static Object[] ForwardViterbi()
+        {
+            return null;
+        }
+    }
+}
