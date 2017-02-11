823f841f7a2e434b69a9da688ee1d5354b61fd49
Sergey <seruy5432@gmail.com>
2017-02-11 22:05:50 +0200

    Initialize data types

--- CodingDNA/CodingDNA/Program.cs
+++ CodingDNA/CodingDNA/Program.cs
@@ -19,10 +19,44 @@ namespace CodingDNA
 
         static void Main(string[] args)
         {
-            //TODO implement initialization of data and its output
+            char[] states = { H, L };
+            char[] observations = { A, C, G, T };
+
+            var startProbability = new Dictionary<char, float>();
+            startProbability.Add(H, 0.5f);
+            startProbability.Add(L, 0.5f);
+
+            //transition prob.
+            var transitionProbability = new Dictionary<char, Dictionary<char, float>>();
+            var HT = new Dictionary<char, float>();
+            HT.Add(H, 0.5f);
+            HT.Add(L, 0.5f);
+            var LT = new Dictionary<char, float>();
+            LT.Add(H, 0.4f);
+            LT.Add(L, 0.6f);
+            transitionProbability.Add(H, HT);
+            transitionProbability.Add(L, LT);
+
+            //emission prob.
+            var emissionProbability = new Dictionary<char, Dictionary<char, float>>();
+            var HE = new Dictionary<char, float>();
+            HE.Add(A, 0.2f);
+            HE.Add(C, 0.3f);
+            HE.Add(G, 0.3f);
+            HE.Add(T, 0.2f);
+            var LE = new Dictionary<char, float>();
+            LE.Add(A, 0.3f);
+            LE.Add(C, 0.2f);
+            LE.Add(G, 0.2f);
+            LE.Add(T, 0.3f);
+            emissionProbability.Add(H, HE);
+            emissionProbability.Add(L, LE);
         }
 
-        public static Object[] ForwardViterbi()
+        public static Object[] ForwardViterbi(string obs, char[] states, 
+                                              Dictionary<char[], float> start_p, 
+                                              Dictionary<char[], Dictionary<char[], float>> trans_p,
+                                              Dictionary<char[], Dictionary<char[], float>> emit_p)
         {
             return null;
         }
